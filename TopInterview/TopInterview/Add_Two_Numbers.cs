72b9d8e619e2edaf2f1350d6d93f62467349c09a
Никита Глушаченко <rylonoboev2004@gmail.com>
2023-06-18 14:55:01 +0300

    Top interview tasks - Two Numbers Addition - Sum of two

---
+++ TopInterview/TopInterview/Add_Two_Numbers.cs
@@ -0,0 +1,43 @@
+﻿namespace TopInterview
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode? next;
+        public ListNode(int val = 0, ListNode? next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode? AddTwoNumbers(ListNode? l1, ListNode? l2)
+        {
+            var head = new ListNode();
+            var curr = head;
+            var carry = 0;
+
+            while (l1 != null || l2 != null)
+            {
+                var sum = carry + (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0);
+                var num = sum % 10;
+                carry = sum / 10;
+
+                curr.next = new ListNode(num);
+                curr = curr.next;
+                l1 = l1?.next;
+                l2 = l2?.next;
+            }
+
+            if (carry > 0)
+            {
+                curr.next = new ListNode(carry);
+            }
+
+
+            return head.next;
+        }
+    }
+}
---
+++ TopInterview/TopInterview/Sum.cs
@@ -0,0 +1,26 @@
+﻿namespace TopInterview
+{
+    public class Sum
+    {
+        public int[]? TwoSum(int[] nums, int target)
+        {
+            var pairs = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var res = 0;
+
+                if (pairs.TryGetValue(target - nums[i], out res))
+                {
+                    return new int[] { res, i };
+                }
+                else
+                {
+                    pairs.TryAdd(nums[i], i);
+                }
+            }
+
+            return default;
+        }
+    }
+}
